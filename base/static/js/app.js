2be2f8cef112d4b0f4c4205c738d96349a078ba6
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-04-26 13:51:40 -0700

    fix colors

--- base/static/js/app.js
+++ base/static/js/app.js
@@ -5,11 +5,11 @@
     console.log("Hola mundo")
     window.addEventListener('scroll', () => {
         if (window.scrollY > threshold) {
-            navbar.classList.add('fixed', 'z-50', 'top-0',  'w-full', 'transform','bg-red-600', 'text-white', 'transition-transform', 'duration-300','ease-in-out'); 
+            navbar.classList.add('fixed', 'z-50', 'top-0',  'w-full', 'transform','bg-indigo-800', 'text-white', 'transition-transform', 'duration-300','ease-in-out'); 
             cart.classList.add('text-white')
             cart.classList.remove('text-indigo-600')
         } else {
-            navbar.classList.remove('fixed', 'z-50', 'top-0',  'w-full', 'transform','bg-red-600', 'text-white', 'transition-transform', 'duration-300','ease-in-out');
+            navbar.classList.remove('fixed', 'z-50', 'top-0',  'w-full', 'transform','bg-indigo-800', 'text-white', 'transition-transform', 'duration-300','ease-in-out');
             cart.classList.remove('text-white')
         }
     });
