62e1000cffe270f9e1eab77b577a43bac27481e4
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-04-27 19:56:59 -0700

    fix date

--- reviews/models.py
+++ reviews/models.py
@@ -1,22 +1,34 @@
 from django.db import models
 from Auth.models import User
 from Products.models import Product
+import pytz
+from django.utils import timezone
 
 # Create your models here.
 class Reviews(models.Model):
     STAR_CHOICES = [
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5'),
+        (1, "1"),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5"),
     ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     comment = models.TextField(max_length=1000)
     stars = models.IntegerField(choices=STAR_CHOICES, null=True)
-    created = models.DateTimeField(auto_now_add=True, null=True)
+    created = models.DateField(auto_now_add=True, null=True)
+
     class Meta:
-        verbose_name  = 'review'
-        verbose_name_plural = 'reviews'
-        db_table ='reviews'
+        verbose_name = "review"
+        verbose_name_plural = "reviews"
+        db_table = "reviews"
+
+    def save(self, *args, **kwargs):
+        if self.created:
+
+            zone_hour = pytz.timezone("America/Los_Angeles")
+
+            self.created = timezone.localtime(self.created, timezone=zone_hour)
+
+        super().save(*args, **kwargs)
