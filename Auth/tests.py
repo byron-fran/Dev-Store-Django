11f3534933e3cc37dad849a061070da0d9d8845e
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-05-02 14:40:37 -0700

    test auth

--- Auth/tests.py
+++ Auth/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+
+class UserTest(TestCase):
+    
+    def setUp(self) -> None:
+        self.user = User.objects.create(username="jhon", email="jhon@gmail.com", password="jhon123")
+        
+    def test_create_user(self):
+        
+        self.assertEqual(self.user.username, 'jhon')    
+        self.assertEqual(self.user.email,'jhon@gmail.com')
+        self.assertFalse(self.user.check_password('jhon123'))
+        
+    def test_login_user(self):
+        self.assertEqual(self.user.email, 'jhon@gmail.com')
+        self.assertFalse(self.user.check_password('jhon123'))
