0a043c82964118273b339657a37104e47697caca
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-06-04 07:55:56 -0700

    add filter price

--- Products/filters.py
+++ Products/filters.py
@@ -2,8 +2,8 @@ from .models import Product
 import django_filters
 
 class ProductFilter(django_filters.FilterSet):
-
+    price = django_filters.NumberFilter()
     class Meta:
         model = Product
-        fields = [ 'category', 'mark']
+        fields = [ 'category', 'mark', 'price']
     
